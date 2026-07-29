0237e74ce2f5cf86f12a2e3316e4dcf363196ae1
Sohail khan <120656530+sohail-68@users.noreply.github.com>
2026-07-29 11:16:43 +0530

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -1,13 +1,11 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: process.env.EMAIL_PORT || 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 exports.sendOTPEmail = async (email, otp, name) => {
